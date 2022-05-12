b154ef2aaa8f04e183642d449e4b6b20f109466a
Sichkar Yurii <si4kar2017@gmail.com>
2022-05-12 14:17:35 +0300

    Lesson_13

--- scripts.js
+++ scripts.js
@@ -3,24 +3,21 @@
 const calc = new Calculator(10);
 
 function Calculator(base) {
-	
-  this.sum = (b) => (base += b),
-  
-  this.mult = (b) => (base *= b),
-
-  this.sub = (b) => (base -= b),
-
-  this.div = (b) => (base /= b),
-  
-  this.set = (b) => (base = b),
-  
-  this.result = () => (base)    
+    this.result = base;
 }
 
-console.log (calc.sum(5));
-console.log (calc.mult(10));
-console.log (calc.sub(40));
-console.log(calc.div(10));
-calc.set(11);
-
-console.log(calc.result());
+Calculator.prototype.add = function (b) {
+    return (this.result += b);
+};
+Calculator.prototype.sub = function (b) {
+    return (this.result -= b);
+};
+Calculator.prototype.mult = function (b) {
+    return (this.result *= b);
+};
+Calculator.prototype.div = function (b) {
+    return (this.result /= b);
+};
+Calculator.prototype.set = function (b) {
+    return (this.result = b);
+};
